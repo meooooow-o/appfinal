97ad32801fb69c740e1572c5c67064b7ae83bcea
meooooow-o <410570126@gapp.fju.edu.tw>
2022-12-20 22:41:38 +0800

    add main3

--- app/src/main/java/com/example/myapplicationwater/MainActivity3.kt
+++ app/src/main/java/com/example/myapplicationwater/MainActivity3.kt
@@ -3,7 +3,7 @@ package com.example.myapplicationwater
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.widget.*
 
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +13,19 @@ class MainActivity3 : AppCompatActivity() {
         findViewById<Button>(R.id.btn_back1).setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java)
             startActivityForResult(intent, 11)
+        }
+
+        val ed_name = findViewById<EditText>(R.id.ed_name)
+        val ed_height = findViewById<EditText>(R.id.ed_height)
+        val ed_weight = findViewById<EditText>(R.id.ed_weight)
+        val btn_enter = findViewById<Button>(R.id.btn_enter)
+        var tv_amount = findViewById<TextView>(R.id.tv_amount)
+        btn_enter.setOnClickListener {
+            var waterAmount = ed_weight.text.toString().toInt() * 30
+            ed_name.text = null
+            ed_height.text = null
+            ed_weight.text = null
+            tv_amount.text = waterAmount.toString() + " ml / 天"
         }
     }
 }
