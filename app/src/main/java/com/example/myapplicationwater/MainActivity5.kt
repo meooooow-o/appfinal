a45e81de89046662e71b319937869badc58901b3
meooooow-o <410570126@gapp.fju.edu.tw>
2022-12-22 23:06:58 +0800

    final

--- app/src/main/java/com/example/myapplicationwater/MainActivity5.kt
+++ app/src/main/java/com/example/myapplicationwater/MainActivity5.kt
@@ -1,6 +1,7 @@
 package com.example.myapplicationwater
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
@@ -13,6 +14,11 @@ class MainActivity5 : AppCompatActivity() {
         findViewById<Button>(R.id.btn_back3).setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java)
             startActivityForResult(intent, 13)
+        }
+        findViewById<Button>(R.id.btn_more).setOnClickListener {
+            val uri = Uri.parse("https://www.youtube.com/results?search_query=%E5%96%9D%E6%B0%B4%E7%9F%A5%E8%AD%98")
+            val i = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(i)
         }
     }
 }
